4bf1216441079aea0f5db1d391bcb97db5cc7ead
Nicolnegg <princesnegg@gmail.com>
2023-05-22 13:12:40 -0500

    chat

--- src/app/Pages/chat/chat.component.ts
+++ src/app/Pages/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Apollo, gql } from 'apollo-angular';
 import { Component, OnInit } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 const GET_MENSAJES = gql`
 query {
@@ -52,6 +54,8 @@ mutation {
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent  implements OnInit {
+  @ViewChild("content") content: any;
+
   mensajes: any[] = [];
   remitentes: any[] = [];
   usuario = 76;
@@ -77,11 +81,14 @@ export class ChatComponent  implements OnInit {
         this.remitentes = result.data?.allMessageByUserId.map(
           (item: any) => item.Remitente
         );
+        this.scrollToBottom();
       });
+    
   }
 
   sendMessage() {
     if (this.newMessage.trim() !== '') {
+      const currentDateTime = new Date().toISOString(); // Obtiene la fecha y hora actual en formato ISO string
       this.apollo
         .mutate({
           mutation: SEND_MENSAJES,
@@ -96,10 +103,14 @@ export class ChatComponent  implements OnInit {
         })
         .subscribe(({ data }: any) => {
           this.mensajes.push(this.newMessage);
+          this.remitentes.push("Usuario")
           this.changeDetectorRef.detectChanges();
           this.newMessage = '';
+          this.changeDetectorRef.detectChanges();
+          this.scrollToBottom();
         });
     }
+    
   }
   getClaseRemitente(i: number): string {
     if (this.remitentes[i] === "Administrador") {
@@ -110,5 +121,10 @@ export class ChatComponent  implements OnInit {
       return '';
     }
   }
+  scrollToBottom() {
+    var contentEND=document.getElementById("content-end").offsetTop;
+    this.content.scrollByPoint(0,contentEND);
+  }
+
 
 }
